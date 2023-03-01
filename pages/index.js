d7eadebe92b140cd1f3fd57aa541ad5b35014d60
devsilva <jerson.silva99@hotmail.com>
2023-02-28 22:32:16 -0500

    feat: Create header component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 
+import Header from '@/components/Header'
+
 export default function Home () {
   return (
     <>
@@ -10,7 +12,7 @@ export default function Home () {
         <link rel='icon' href='/favicon.png' />
       </Head>
 
-      <h1>Home page</h1>
+      <Header />
     </>
   )
 }
